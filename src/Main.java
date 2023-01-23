0ce70e6f1c261b209160d03116338933f6825fee
Nikita Rochev <nikita.rochev@teamup-soft.ru>
2023-01-23 21:07:28 +0300

    Пример работы с проверяемыми исключениями

--- src/Main.java
+++ src/Main.java
@@ -1,39 +1,36 @@
-import java.util.Arrays;
-import java.util.Scanner;
+import java.io.IOException;
+import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-        int number;
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Введи номер от 1 до 5");
+        //try - блок в котором ожидается ошибка
+        //catch - блок в котором обрабатывается ошибка
+        //throw - который принудительно вызывает ошибку
+        //throws - Позволяет пометить метод и предупредить о том,
+        // что внутри метода может быть вызвана ошибка
 
-        int a = scanner.nextInt();
-        try {
-            switch (a) {
-                case 1:
-                    throw new RuntimeException("Отработала единичка");
-                case 2:
-                    throw new IllegalArgumentException("Отработала двоичка");
-                case 3:
-                    throw new NumberFormatException("Отработала Троичка");
-                case 4:
-                    throw new UserNotFoundException("3578");
-                case 5:
-                    throw new UserNotFoundException("3578", "Maxim");
+        String test = "Test";
+        Random random = new Random();
+        for (int i = 0; i < 10; i++) {
+            try {
+                connect(random.nextInt(10));
+                return;
+            } catch (IOException e) {
+                System.out.println("Получили ошибку соединение. Пытаемся сделать заного");
             }
-        } catch (UserNotFoundException e) { // При работе с несколькими Catch, очень важно соблюдать иерархию наследования.
-            // Потому-что catch ловит саму ошибку и всех её наследников
-            System.out.println(e.getMessage());
-        } catch (NumberFormatException ex) {
-            System.out.println(ex.getMessage());
-            System.out.println(Arrays.toString(ex.getStackTrace()));
-        } catch (IllegalArgumentException exception) {
-            System.out.println("Что-нибудь да хотим!");
-        } catch (Exception e) {
-            System.out.println("Где-то там, далеко. Что-то произошло, что я не знал");
         }
 
-        System.out.println("Мы ещё живы и работаем!");
+        System.out.println("К сожалению за 10 попыток не удалось сделать соединение");
 
+    }
+
+    public static void connect(int a) throws IOException {
+        System.out.println("Попытка получения соединения");
+
+        if (a == 1) {
+            System.out.println("Удачное соединение");
+        } else {
+            throw new IOException();
+        }
     }
 }
